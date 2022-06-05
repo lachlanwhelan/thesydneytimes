e6a1d74b9d7ccb71810dbf7b4b563be0889dabe6
Lachlan Whelan <lachlan.whelan1@gmail.com>
2022-06-05 17:06:24 +1000

    Some cleanup

--- js/main.js
+++ js/main.js
@@ -14,17 +14,13 @@ const setDate = async () => {
 const setMarket = async () => {
     const marketRow = document.querySelector(".market-row");
 
-    //marketRow.innerHTML = `<p class="stock-item">All Ords +1.03%</p>`
-    //MAX 100 requests per day
-
-        //yahoo finance api
+    //yahoo finance api
     const stockResponse = await fetch(`https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=%5EAORD%2C%5EAXJO%2CAUDUSD%3DX%2CBTC-AUD`,{
         method: "GET",
         headers: { 'x-api-key': stockKey},
     });
 
     const stockData = await stockResponse.json();
-    //console.log(stockData);
 
     if(stockData.message){
         marketRow.innerHTML = `<p>Stock Data</p>`;
@@ -51,15 +47,15 @@ const setMarket = async () => {
 const setWeather = async () => {
     const weatherRow = document.querySelector(".weather-row");
 
+    //ip geolocation api - to get coords to use in weather api
     const coordResponse = await fetch(`https://ipapi.co/json/`);
     const coords = await coordResponse.json();
     const {latitude, longitude} = coords;
 
+    //open weather api
     const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=metric`);
     const weatherData = await weatherResponse.json();
     const {main, weather} = weatherData;
-    //http://openweathermap.org/img/wn/10d@2x.png
-   // console.log(main, weather);
 
     const html = `
         <p title="Today's Weather: ${main.temp.toFixed(0)}°C and ${weather[0].main}"><img class="weather-icon" src="http://openweathermap.org/img/wn/${weather[0].icon}.png"/></p>
@@ -72,7 +68,10 @@ const setWeather = async () => {
 };
 
 export const setTopStories = async (category) => {
+
     setLoader(true);
+
+    //nytimes api
     const newsResponse = await fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${newsKey}`);
 
     const newsData = await newsResponse.json();
@@ -108,10 +107,34 @@ export const setTopStories = async (category) => {
         
     }
 
+    //slowing down loader so images can load up before
     setTimeout(() => {
         setLoader(false);
     }, 3000);
 };
+
+export const getSearchResults = async (query) => {
+
+    setLoader(true);
+    
+    //nytimes api - search endpoint
+    const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${newsKey}`);
+
+    const results = await response.json();
+
+    const mainFeed = document.querySelector(".main-feed");
+
+    results.response.docs.forEach((article, index) => {
+        console.log(article);
+        mainFeed.innerHTML += createArticleTemplate(article, null, "SEARCH");   
+    });
+
+    //slowing down loader so images can load up before
+    setTimeout(() => {
+        setLoader(false);
+    }, 3000);
+
+}
 
 const setLoader = (loadStatus) => {
     const loadScreen = document.querySelector(".loader-screen");
@@ -126,7 +149,8 @@ const setLoader = (loadStatus) => {
 const createArticleTemplate = (article, number, type) => {
 
 
-    if(type === "TOPFIVE"){
+    if (type === "TOPFIVE")
+    {
         return `
         <a class="news-article article-${number + 1}" href=${article.url} style="background-image: url(${ article.multimedia ? article.multimedia[0].url : null})">
             <div class="news-article-text">
@@ -137,8 +161,8 @@ const createArticleTemplate = (article, number, type) => {
         `;
     }
 
-    if(type === "MAINFEED"){
-        
+    if (type === "MAINFEED") 
+    {
         return `
         <a class="news-article bar-template-article" href=${article.url}>
             <div class="news-article-text">
@@ -154,7 +178,8 @@ const createArticleTemplate = (article, number, type) => {
         `;
     }
 
-    if(type === "SIDEFEED"){
+    if (type === "SIDEFEED") 
+    {
         return `
         <a class="news-article strip-template-article" href=${article.url}>
             <div class="news-article-text">
@@ -168,7 +193,8 @@ const createArticleTemplate = (article, number, type) => {
         `;
     }
 
-    if(type === "SEARCH"){
+    if (type === "SEARCH") 
+    {
         return `
         <a class="news-article bar-template-article" href=${article.web_url}>
             <div class="news-article-text">
@@ -184,22 +210,6 @@ const createArticleTemplate = (article, number, type) => {
         `;
     }
     
-}
-
-
-export const getSearchResults = async (query) => {
-    
-    const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${newsKey}`);
-
-    const results = await response.json();
-
-    const mainFeed = document.querySelector(".main-feed");
-
-    results.response.docs.forEach((article, index) => {
-        console.log(article);
-        mainFeed.innerHTML += createArticleTemplate(article, null, "SEARCH");   
-    });
-
 }
 
 
